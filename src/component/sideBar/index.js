df5134c353cc4ed4b32c615f74dcc813444356a9
hoangcuteday <109096454+hoangcuteday@users.noreply.github.com>
2023-05-27 13:27:32 +0700

    push -1

--- src/component/sideBar/index.js
+++ src/component/sideBar/index.js
@@ -1,10 +1,18 @@
+import styles from "./SideBar.scss";
+import classNames from 'classnames/bind';
+
 import FeatureList from "../featureList";
 import UserInfo from "../userInfo";
+
+const cx = classNames.bind(styles);
+
 
 function SideBar() {
     return (
         <div className="w-full lg:h-screen bg-green-100 flex lg:flex-col justify-between">
-            <FeatureList />
+            <div className={cx("list")}>
+                <FeatureList />
+            </div>
             <UserInfo />
         </div>
     );
